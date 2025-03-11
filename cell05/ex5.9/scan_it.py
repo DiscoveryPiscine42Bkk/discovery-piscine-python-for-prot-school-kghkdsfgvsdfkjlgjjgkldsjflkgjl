08d3c88496fadd96ef5022df92d79f82ee28ef73
kghkdsfgvsdfkjlgjjgkldsjflkgjl <thanapolmalikho4@gmail.com>
2025-03-11 14:26:52 +0700

    Add files via upload

---
+++ cell05/ex5.9/scan_it.py
@@ -0,0 +1,14 @@
+import sys
+
+if len(sys.argv) == 3:
+    keyword = sys.argv[1]
+    search_string = sys.argv[2]
+    
+    count = search_string.count(keyword)
+    
+    if count > 0:
+        print(count)
+    else:
+        print('none')
+else:
+    print('none')
