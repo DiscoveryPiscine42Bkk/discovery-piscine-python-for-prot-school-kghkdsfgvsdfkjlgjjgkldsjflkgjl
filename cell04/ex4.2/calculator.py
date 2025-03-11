7c76d62f6653001efffb494ddbe739d189502266
kghkdsfgvsdfkjlgjjgkldsjflkgjl <thanapolmalikho4@gmail.com>
2025-03-11 12:58:33 +0700

    Add files via upload

---
+++ cell04/ex4.2/calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def main():
+    num1 = float(input("Give me the first number: "))
+    num2 = float(input("Give me the second number: "))
+    
+    print("Thank you!")
+    print(f"{num1} + {num2} = {num1 + num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
+    print(f"{num1} / {num2} = {num1 / num2}")
+    print(f"{num1} * {num2} = {num1 * num2}")
+
+if __name__ == "__main__":
+    main()
