2e832be33c00016ac69ed40da4675aaba8ebc3f6
kghkdsfgvsdfkjlgjjgkldsjflkgjl <thanapolmalikho4@gmail.com>
2025-03-11 15:30:23 +0700

    Add files via upload

---
+++ cell06/ex6.2/downcase_all.py
@@ -0,0 +1,10 @@
+import sys
+
+def downcase_it(s):
+    return s.lower()
+
+if len(sys.argv) == 1:
+    print('none')
+else:
+    for param in sys.argv[1:]:
+        print(downcase_it(param))
---
+++ cell06/ex6.5/scope_that.py
@@ -0,0 +1,10 @@
+def add_one(n):
+    return n + 1
+
+num = 5
+
+print(f'Before: {num}')
+
+num = add_one(num)
+
+print(f'After: {num}')
